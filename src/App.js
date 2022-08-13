7403125c3b2cb43b8a5cc9961da77fde302f7585
ivandjoh <ivandi.djoh@gmail.com>
2022-08-14 00:30:00 +0800

    conflict

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
 import Home from './pages/Home';
 import Data from './pages/Data';
@@ -13,11 +12,6 @@ function App() {
     <>
       <Router>
         <Routes>
-          {/* <ul>
-            <li><Link to="/events">Events</Link></li>
-            <li><Link to="/date">Date</Link></li>
-            <li><Link to="/accueil">Accueil</Link></li>
-          </ul> */}
           <Route path='/' element={<Home />}>Home</Route>
           <Route path='/data' element={<Data />}>Data</Route>
           <Route path='*' element={<h4>Page Not Found!</h4>} />
